5056c8fb13e073c7a428dabe54f639c9775f290d
babak-ab <ba.ghasemi0@gmail.com>
2018-10-17 21:40:59 +0330

    DOwnload Info Dialog

---
+++ app/src/main/java/com/example/babak/baresh/DownloadInfoDialog.java
@@ -0,0 +1,49 @@
+package com.example.babak.baresh;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DownloadInfoDialog extends Dialog {
+
+    public DownloadInfoDialog(@NonNull Context context,String url) {
+        super(context);
+        setContentView(R.layout.download_info_dialog_layout);
+        setTitle("Add link for download...");
+        List<String> categories = new ArrayList<String>();
+        categories.add("General");
+        categories.add("Archives");
+        categories.add("Documents");
+        categories.add("Musics");
+        categories.add("Videos");
+        categories.add("Programs");
+        // String[] values = {"General","Archives","Documents","Musics","Videos","Programs"};
+
+        final Spinner spinner = (Spinner) this.findViewById(R.id.spinner_downloadInfoGroups);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context,
+                android.R.layout.simple_spinner_item, categories);
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        final EditText editText = (EditText)this.findViewById(R.id.editText_downloadInfoUrl);
+        editText.setText(url);
+        //spinner.setAdapter(new GroupsArrayAdapter(MainActivity.this, categories.toArray(new String[0])));
+    }
+    public void setFileType(int type){
+
+    }
+    public void setFileSize(int size){
+
+    }
+}
